f48ab25994a462d78d7e6aa0d6c1cf6b094d2ec2
fowlera860 <fowlera860@outlook.com>
2025-10-13 12:25:51 -0500

    updated polling

--- taskpane.js
+++ taskpane.js
@@ -1,28 +1,23 @@
-Office.onReady((info) => {
-    if (info.host === Office.HostType.Excel) {
-        pollForUpdates();
-    }
+Office.onReady(() => {
+    pollSidebarMessage();
 });
 
-
-async function pollForUpdates() {
+async function pollSidebarMessage() {
     try {
         await Excel.run(async (context) => {
             const sheet = context.workbook.worksheets.getActiveWorksheet();
-            const range = sheet.getRange("SidebarMessage");
+            const range = sheet.names.getItem("SidebarMessage").getRange();
             range.load("values");
             await context.sync();
 
             const msg = range.values[0][0];
             if (msg) {
-                // Update the task pane content
                 document.getElementById("content").innerText = msg;
             }
         });
     } catch (error) {
         console.error(error);
     } finally {
-        // poll every 1 second
-        setTimeout(pollForUpdates, 1000);
+        setTimeout(pollSidebarMessage, 1000); // poll every 1 second
     }
 }
